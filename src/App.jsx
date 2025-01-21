155c8afb777fedb62e9ee95534964cdee66f38a1
JUAN_HUN <juanhun2424@gmail.com>
2025-01-21 15:40:21 -0500

    add ScrollToTop function/component to fix the bug that kept the user at the bottom of the page upon click another tab

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Footer from "./components/Footer"
 import Navbar from "./components/Navbar"
 import { Outlet } from 'react-router-dom'
+import ScrollToTop from "./components/ScrollToTop"
 
 function App() {
 
   return (
     <>
+      <ScrollToTop />
       <Navbar />
       <Outlet />
       <Footer />
