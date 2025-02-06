d86d49de0c1c06d3aae8a4dd85ec7151a63091d6
JUAN_HUN <juanhun2424@gmail.com>
2025-02-06 10:27:26 -0500

    Add WorkInfoCard component with 3 projs info

---
+++ src/components/WorkInfoCard.jsx
@@ -0,0 +1,57 @@
+import Reveal from "./Reveal";
+
+
+export default function WorkInfoCard({title, description, bulletPoints, videoSrcLink, githubRepoLink, youtubeLink}){
+  return (
+    <div>
+      <Reveal>
+      <div className="card relative self-center w-[63rem] h-[30rem] mt-[10rem] lg:card-side bg-primary">
+        <figure className="w-1/3">
+            <iframe
+              width="100%"
+              height="315"
+              src={videoSrcLink}
+              style={{ border: 0 }}
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+            ></iframe>
+          </figure>
+          <div className="card-body w-2/3">
+            <h2 className="card-title"> {title} </h2>
+            <h3 className="font-bold italic"> {description} </h3>
+            <ul >
+              {
+                bulletPoints ?
+                  bulletPoints.map((bullet, index) => {
+                    return <li key={index} className="my-2" >{bullet}</li>
+                  }) 
+                : <h1>There are none</h1>
+              }
+            </ul>
+            <br/>
+            <div className="card-actions absolute justify-end bottom-10 right-10">
+              <button className="btn btn-secondary">
+                <a
+                  href={githubRepoLink}
+                  target="_blank"
+                  rel="noopener noreferrer"  
+                  >
+                  Github
+                </a>
+              </button>
+              <button className="btn btn-secondary">
+                <a
+                  href={youtubeLink}
+                  target="_blank"
+                  rel="noopener noreferrer"  
+                  >
+                  YouTube
+                </a>
+              </button>
+            </div>
+          </div>
+        </div>
+      </Reveal>
+    </div>
+  )
+}
--- src/pages/WorkPage.jsx
+++ src/pages/WorkPage.jsx
@@ -3,9 +3,53 @@ import welcomeGuyAnimationData from '../assets/animations/welcome-guy-animation.
 import speechBubble from '../assets/speech-bubble.svg'
 import Reveal from '../components/Reveal'
 import { Link } from 'react-router-dom'
+import WorkInfoCard from '../components/WorkInfoCard'
 
 export default function WorkPage() {
   
+  const project1Info = {
+    title : 'Redzone Getaway (Group Project)',
+    description : "This is a travel booking site made for NFL sans who want to keep up with their teams latest scores and follow them around the country, never missing a single live game!",
+    bulletPoints : [
+      "Users can create, update, and delete events, flights, and hotel bookings.",
+      "Users can look up flights and hotels by location code.",
+      "Users can add items to their cart so they can continue browsing and proceed to checkout when they are ready.",
+      "Built on React.js and Django , utilizing the Ball Don't Lie API for NFL player stats, Sports Data API for NFL game scores, Flight API for real-time flight data, and the Amadeus API for real-time hotel data."
+    ],
+    videoSrcLink : "https://www.youtube.com/embed/woOCsWDlof0",
+    githubRepoLink : "https://github.com/juanHunOfficial/redzone-getaway-clone",
+    youtubeLink : "https://youtu.be/woOCsWDlof0",
+  }
+
+  const project2Info = {
+    title : 'Progressive Overload Tracker (First Solo Project)',
+    description : "This is a web application for bodybuilders that want to track their progress effectively",
+    bulletPoints : [
+      "Users can create, update, and delete trackers, workouts, exercises, and data points showcasing their progress.",
+      "Users can look up exercises by body part to get inspiration for their workouts.",
+      "Users can look up meals to get insight into the meal’s macro-nutrients to help them meet their nutrition goals.",
+      "Built on React.js and Django , utilizes Ninja-API’s Nutrition API and Exercise API for nutrition and exercise data."
+    ],
+    videoSrcLink : "https://www.youtube.com/embed/aSZmQHxTa5w",
+    githubRepoLink : "https://github.com/juanHunOfficial/code-platoon-folder/tree/main/fitness_proj",
+    youtubeLink : "https://youtu.be/aSZmQHxTa5w",
+  }
+
+  const project3Info = {
+    title : 'Coming Soon, the button below takes you to the repository of my latest project',
+    description : "This is a web application for bodybuilders that want to track their progress effectively",
+    bulletPoints : [
+      `Currently I am learning how to use Node.js, Express, and MongoDB by making a finance app. The goal of the app 
+        is to help my wife and I keep better track of our money with an app that can link to our bank account and pull 
+        all of our recent transactions. This will allow us to categorize our spending in the appropriate categories and 
+        keep track of the our spending habits. The categories and their subsequent data will be saved to the account and 
+        displayed in a graph utilizing charts.js to make an easy to understand visual representation of our spending habits.`
+    ],
+    videoSrcLink : "",
+    githubRepoLink : "https://github.com/juanHunOfficial/finance-app",
+    youtubeLink : "",
+  }
+
   return(
     <>
       <div className='flex flex-col items-center justify-center'>
@@ -40,133 +84,34 @@ export default function WorkPage() {
         <h1 className='text-6xl mt-[10rem] divider divider-secondary'>Full-Stack Projects</h1>
       </Reveal>
       {/* Projects showcase */}
-      <div className='flex flex-col items-center justify-center'>
+        <div className='flex flex-col items-center my-[10rem] justify-center'>
         {/* Project 1 */}
-        <Reveal>
-        <div className="card self-center w-[63rem] h-[25rem] mt-[10rem] lg:card-side bg-primary">
-        <figure className="w-1/3">
-            <iframe
-              width="100%"
-              height="315"
-              src="https://www.youtube.com/embed/woOCsWDlof0"
-              title="YouTube video player"
-              style={{ border: 0 }}
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-              allowFullScreen
-            ></iframe>
-          </figure>
-          <div className="card-body w-2/3">
-            <h2 className="card-title">Redzone Getaway (Group Project)</h2>
-            <ul>
-              <li>Users can create, update, and delete events, flights, and hotel bookings.</li>
-              <li>Users can look up flights and hotels by location code.</li>
-              <li>Users can add items to their cart so they can continue browsing and proceed to checkout when they are ready.</li>
-              <li>Built on React.js and Django , utilizing the Ball Don't Lie API for NFL player stats, Sports Data API for NFL game 
-                scores, Flight API for real-time flight data, and the Amadeus API for real-time hotel data.
-              </li>
-            </ul>
-            <br/>
-            <div className="card-actions justify-end">
-              <button className="btn btn-secondary">
-                <a
-                  href='https://github.com/juanHunOfficial/redzone-getaway-clone'
-                  target="_blank"
-                  rel="noopener noreferrer"  
-                  >
-                  Github
-                </a>
-              </button>
-              <button className="btn btn-secondary">
-                <a
-                  href='https://youtu.be/woOCsWDlof0'
-                  target="_blank"
-                  rel="noopener noreferrer"  
-                  >
-                  YouTube
-                </a>
-              </button>
-            </div>
-          </div>
-        </div>
-        </Reveal>
+          <WorkInfoCard 
+            title={project1Info.title} 
+            description={project1Info.description}
+            bulletPoints={project1Info.bulletPoints} 
+            videoSrcLink={project1Info.videoSrcLink} 
+            githubRepoLink={project1Info.githubRepoLink} 
+            youtubeLink={project1Info.youtubeLink} 
+          />
           {/* Project 2 */}
-        <Reveal>
-        <div className="card lg:card-side self-center w-[63rem] h-[25rem] mt-[10rem] bg-primary">
-          <figure className="w-1/3">
-            <iframe
-              width="100%"
-              height="315"
-              src="https://www.youtube.com/embed/aSZmQHxTa5w"
-              title="YouTube video player"
-              style={{ border: 0 }}
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-              allowFullScreen
-            ></iframe>
-          </figure>
-          <div className="card-body w-2/3">
-            <h2 className="card-title">Progressive Overload Tracker (First Solo Project)</h2>
-            <title>This is a web application for bodybuilders that want to track their progress effectively</title>
-            <ul>
-              <li>Users can create, update, and delete trackers, workouts, exercises, and data points showcasing their progress.</li>
-              <li>Users can look up exercises by body part to get inspiration for their workouts.</li>
-              <li>Users can look up meals to get insight into the meal’s macro-nutrients to help them meet their nutrition goals.</li>
-              <li>Built on React.js and Django , utilizes Ninja-API’s Nutrition API and Exercise API for nutrition and exercise data.</li>
-            </ul>
-            <br/>
-            <div className="card-actions justify-end ">
-              <button className="btn btn-secondary">
-                <a
-                  href='https://github.com/juanHunOfficial/code-platoon-folder/tree/main/fitness_proj'
-                  target="_blank"
-                  rel="noopener noreferrer"  
-                  >
-                  Github
-                </a>
-              </button>
-              <button className="btn btn-secondary">
-                <a
-                  href='https://youtu.be/aSZmQHxTa5w'
-                  target="_blank"
-                  rel="noopener noreferrer"  
-                  >
-                  YouTube
-                </a>
-              </button>
-            </div>
-          </div>
-        </div>
-        </Reveal>
+          <WorkInfoCard 
+            title={project2Info.title} 
+            description={project2Info.description}
+            bulletPoints={project2Info.bulletPoints} 
+            videoSrcLink={project2Info.videoSrcLink} 
+            githubRepoLink={project2Info.githubRepoLink} 
+            youtubeLink={project2Info.youtubeLink} 
+          />
           {/* Project 3 */}
-        <Reveal>
-        <div className="card lg:card-side self-center w-[63rem] h-[25rem] my-[10rem] bg-primary">
-          <figure >
-            <img
-              src="https://img.daisyui.com/images/stock/photo-1494232410401-ad00d5433cfa.webp"
-              alt="Coming Soon" />
-          </figure>
-          <div className="card-body w-2/3">
-            <h2 className="card-title">Coming Soon, the button below takes you to the repository of my latest project</h2>
-            <p>
-              Currently I am learning how to use Node.js, Express, and MongoDB by making a finance app. The goal of the app 
-              is to help my wife and I keep better track of our money with an app that can link to our bank account and pull 
-              all of our recent transactions. This will allow us to categorize our spending in the appropriate categories and 
-              keep track of the our spending habits. The categories and their subsequent data will be saved to the account and 
-              displayed in a graph utilizing charts.js to make an easy to understand visual representation of our spending habits.
-            </p>
-            <div className="card-actions justify-end">
-              <button className="btn btn-secondary">
-                <a
-                  href='https://github.com/juanHunOfficial/finance-app'
-                  target="_blank"
-                  rel="noopener noreferrer"  
-                  >
-                  Github
-                </a>
-              </button>
-            </div>
-          </div>
-        </div>
-        </Reveal>
+          <WorkInfoCard 
+            title={project3Info.title} 
+            description={project3Info.description}
+            bulletPoints={project3Info.bulletPoints} 
+            videoSrcLink={project3Info.videoSrcLink} 
+            githubRepoLink={project3Info.githubRepoLink} 
+            youtubeLink={project3Info.youtubeLink} 
+          />
       </div>
     </>
   )
